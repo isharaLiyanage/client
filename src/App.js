0f4a417370bf653b495c054a2df46b4d73cf42b8
isharaLiyanage <ishandananjaya1111@gmail.com>
2022-09-06 18:22:08 +0530

    0.1.17

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import "./App.css";
 import React from "react";
-// import {
-//   Routes,
-//   Route,
-//   Navigate,
-//   BrowserRouter as Router,
-// } from "react-router-dom";
-// import Home from "./pages/Home";
-// import ProductList from "./pages/ProductList";
-// import Product from "./pages/product";
-// import Cart from "./pages/Cart";
-// import Login from "./pages/Login";
-// import SignIn from "./pages/SignIn";
-// import UserInfo from "./pages/UserInfo";
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import SignIn from "./pages/SignIn";
+import UserInfo from "./pages/UserInfo";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 function App() {
-  // const user = useSelector((state) => state.user?.currentUser);
+  const user = useSelector((state) => state.user?.currentUser);
   return (
     <div className="App">
       <Container>
-        {/* <Router>
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/client" element={<Navigate to="/" replace />}></Route>
@@ -40,7 +35,7 @@ function App() {
               element={user ? <Navigate replace to="/client" /> : <SignIn />}
             />
           </Routes>
-        </Router> */}
+        </BrowserRouter>
       </Container>
     </div>
   );
